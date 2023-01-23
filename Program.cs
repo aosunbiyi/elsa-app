9632f93d5851475b70cbabfc3b9abc22fcf2ae14
Adebayo Osunbiyi <adebayo.osunbiyi.a@gmail.com>
2023-01-23 08:25:54 +0100

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using Elsa;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace ElsaQuickstarts.ConsoleApp.HelloWorld
+{
+    class Program
+    {
+        private static async Task Main()
+        {
+            var host = new HostBuilder()
+              .ConfigureServices(ConfigureServices)
+              .UseConsoleLifetime()
+              .Build();
+
+            using (host)
+            {
+                await host.StartAsync();
+                await host.WaitForShutdownAsync();
+            }
+        }
+
+        private static void ConfigureServices(IServiceCollection services) =>
+            services
+                .AddElsa(elsa => elsa
+                    .AddConsoleActivities()
+                    .AddQuartzTemporalActivities()
+                    .AddWorkflow<RecurringTaskWorkflow>());
+    }
+}
